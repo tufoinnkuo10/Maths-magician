bdbe7b055b4b8ccfb34f745814fd8523dc289c9c
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-27 11:29:21 +0100

    ad subtraction and buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -21,6 +21,11 @@ class Calculator extends React.Component {
           <button type="button">7</button>
           <button type="button">8</button>
           <button type="button">9</button>
+          <button className="orange-button" type="button">&times;</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button className="orange-button" type="button">&subtract;</button>
         </div>
       </div>
     );
