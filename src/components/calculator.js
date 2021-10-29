54e2432df4248ec4686b2110bf45a84e555b1845
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-10-29 09:54:05 +0100

    change class to function and use state

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
+const calculator () => {
+  const [object, setObject] = useState({});
+
+}
+/* class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-  }
+  }*/
 
+  
+/*
   ifClicked = (e) => {
     const buttonName = e.target.textContent;
     const newObj = calculate(this.state, buttonName);
     this.setState(newObj);
   };
 
-  render() {
+  render() { */
+    
+    
     const buttonNames = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x',
       '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
