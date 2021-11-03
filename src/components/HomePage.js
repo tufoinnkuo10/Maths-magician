749ea5d15fc0e10dd85f77ef79700ffb070aad0f
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-03 09:39:55 +0100

    add quote page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import styles from './Homepage.module.css';
 
 const Homepage = () => (
   <div className={styles.header}>
-    <h2>Welcome to our page</h2>
+    <h1>Welcome to our page</h1>
     <p>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
